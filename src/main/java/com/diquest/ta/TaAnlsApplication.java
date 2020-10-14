0324db80a2a9b9af7c0f101977c779f76f85203a
Dell <jaehyun0803@naver.com>
2020-10-14 17:11:32 +0900

    first commit

---
+++ src/main/java/com/diquest/ta/TaAnlsApplication.java
@@ -0,0 +1,13 @@
+package com.diquest.ta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaAnlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaAnlsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diquest/ta/TaAnlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diquest.ta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaAnlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
