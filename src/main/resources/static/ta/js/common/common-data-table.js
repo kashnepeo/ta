8dbf4245f2c20b7ec101aefa52cfdbe2a1b72111
tamir <gtamir.gt@gmail.com>
2020-10-26 10:51:46 +0900

    비활성화2

--- src/main/resources/static/ta/js/common/common-data-table.js
+++ src/main/resources/static/ta/js/common/common-data-table.js
@@ -3,9 +3,9 @@ $(function() {
 
     $(function() {
         $('#dataTableExample').DataTable({
-            "paging":   false, // previous 1 2 3 next 비활성화
-            "info":     false, // Showing 1 to 10 of 22 entries 비활성화
-            "searching": false // search box 비활성화
+            "paging": false,    // previous 1 2 3 next 비활성화
+            "info": false,      // Showing 1 to 10 of 22 entries 비활성화
+            "searching": false  // search box 비활성화
         });
 
     });
