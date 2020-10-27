9f7a5c8ec4415b14ca5f9a407d0a2553791b1063
tamir <gtamir.gt@gmail.com>
2020-10-27 14:49:58 +0900

    페이징

--- src/main/java/com/diquest/ta/call/controller/CallController.java
+++ src/main/java/com/diquest/ta/call/controller/CallController.java
@@ -3,13 +3,13 @@ package com.diquest.ta.call.controller;
 import com.diquest.ta.call.model.CallEntity;
 import com.diquest.ta.call.repository.CallRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,4 +31,40 @@ public class CallController {
         model.addAttribute("callList", callList);
         return "call/callList :: #dataTableExample_wrapper";
     }
+
+    @GetMapping("/viewCallList2.do")
+    public String viewCallList2(Model model, Pageable pageable) {
+        return "call/callList2";
+    }
+
+
+    @PostMapping("/retrieveCallList2.do")
+    public String retrieveCallList2(Model model,
+                        @PageableDefault(size = 10)  Pageable pageable,
+                        @RequestParam(required = false, defaultValue = "") String searchText) {
+
+        Page<CallEntity> callList = callRepository.findByYyyymmddContaining(searchText, pageable);
+
+        int viewPageSize = 10;
+        long totalElementsNumber = callList.getTotalElements();
+        int totalPagesNumber = callList.getTotalPages();
+        int currentPageNumber = callList.getPageable().getPageNumber();
+        int startPage = 1 * viewPageSize * (currentPageNumber / 10);
+        int endPage = startPage + viewPageSize - 1;
+        int size = callList.getSize();
+
+        model.addAttribute("viewPageSize", viewPageSize);
+        model.addAttribute("totalElementsNumber", totalElementsNumber);
+        model.addAttribute("totalPagesNumber", totalPagesNumber);
+        model.addAttribute("currentPageNumber", currentPageNumber);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("searchText", searchText);
+        model.addAttribute("size", size);
+        model.addAttribute("callList", callList);
+
+        return "call/callList2 :: #dataTableExample_wrapper";
+    }
+
+
 }
--- src/main/java/com/diquest/ta/call/repository/CallRepository.java
+++ src/main/java/com/diquest/ta/call/repository/CallRepository.java
@@ -1,6 +1,7 @@
 package com.diquest.ta.call.repository;
 
 import com.diquest.ta.call.model.CallEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,6 @@ import java.util.List;
 public interface CallRepository extends JpaRepository<CallEntity, String> {
 
     List<CallEntity> findByYyyymmdd(String yyyymmdd, Pageable pageable);
+    Page<CallEntity>findByYyyymmddContaining(String yyyymmdd, Pageable pageable);
 
 }
