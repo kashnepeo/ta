61f76080b13fdcf310116f699e2c52a6f1c68d23
tamir <gtamir.gt@gmail.com>
2020-11-12 16:14:03 +0900

    index번호 추가 및 영어를 한글로 표시

--- src/main/resources/static/ta/js/call/callList.js
+++ src/main/resources/static/ta/js/call/callList.js
@@ -74,15 +74,24 @@ function search(){
         destroy: true,
         processing: true,
         serverSide: true,
-        bSort: true,
         aLengthMenu: [
             [10, 20, 50],
             [10, 20, 50]
         ],
         iDisplayLength: 10,
         language: {
-            search: ""
+            search: "",
+            emptyTable: "데이터가 없습니다",
+            info: "_START_~_END_개(전체 _TOTAL_개 중) 항목 표시",
+            infoEmpty: "0~0개(전체 0개 중) 항목 표시",
+            lengthMenu: "_MENU_ 개씩 보기",
+            processing: "처리중...",
+            paginate: {
+                next: "다음",
+                previous: "이전"
+            },
         },
+
         ajax: {
             url: "/callRest/retrieveCallList.do",
             type: "POST",
@@ -96,26 +105,41 @@ function search(){
             }
         },
         columns: [
+            {data: null, title: "번호", width: "5%",},
             {data: "rfileNm", title: "콜 아이디", width: "20%"},
             {data: "rusrId", title: "상담원 아이디", width: "20%"},
             {data: "rusrNm", title: "상담원 이름", width: "20%"},
             {data: "yyyymmdd", title: "상담일", width: "20%"},
             {data: null, title: "이벤트 발생", width: "8%",},
         ],
-        columnDefs: [{
-            targets: -1,
-            searchable: false,
-            orderable: false,
-            data: null,
-            defaultContent: "<button class=\"btn btn-info\" id=\"click\" type=\"button\">상세보기</button>"
-        }]
+        columnDefs: [
+            {
+                targets: 0,
+                searchable: false,
+                orderable: false,
+            },
+            {
+                targets: -1,
+                searchable: false,
+                orderable: false,
+                data: null,
+                defaultContent: "<button class=\"btn btn-info\" id=\"click\" type=\"button\">상세보기</button>"
+            }
+        ]
     });
+
+    /** index 번호 계산 **/
+    table.on( 'draw.dt', function () {
+        var PageInfo = $('#dataTableCallList').DataTable().page.info();
+        table.column(0, { page: 'current' }).nodes().each( function (cell, i) {
+            cell.innerHTML = i + 1 + PageInfo.start;
+        } );
+    } );
 
     //$('#dataTableCallList tbody').on( 'click', '#click', function () {
     $("#dataTableCallList tbody").off('click').on( 'click', '#click', function (e) {
-            var data = table.row( $(this).parents('tr') ).data();
-            console.log(data);
-            alert(data.rfileNm);
+        var data = table.row( $(this).parents('tr') ).data();
+        window.open("/chat/viewChatList.do?rfileNm="+data.rfileNm, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=500,width=1400,height=1400");
     });
 
 
